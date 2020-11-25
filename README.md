2e6e7dc71420c38697c4ad0394da3bdb080b28fa
mik90 <kaliman.mike@gmail.com>
2020-11-25 11:57:54 -0500

    Writes simple perf text from one pi-hole to display

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Reads pi-hole status and prints it out to eInk display
 Run this on the same board as the pi-hole so that the load data is available
 This can be wired to the pi-hole without sitting directly on the board as a  
 hat (or bonnet, as the brits say)  
-Display pi hole load avg, block percentage, and other stats
+Display pi hole load avg, block percentage, and other stats  
 Needs 8 wires:
 - SCK (SPI)
 - MOSI (SPI)
@@ -13,6 +13,7 @@ Needs 8 wires:
 - D10 (optional)
 - D9 (optional)
 - D5 (optional)
+The font file "font5x8.bin" is pulled from the Adafruit example [here](https://github.com/adafruit/Adafruit_CircuitPython_framebuf/raw/master/examples/font5x8.bin)
 
 ### Display
 - Product page [here](https://www.adafruit.com/product/4687)
@@ -23,3 +24,13 @@ Needs 8 wires:
 - Telnet API [here](https://docs.pi-hole.net/ftldns/telnet-api/)
     - Can use telnetlib for this
 - Python telnetlib docs [here](https://docs.python.org/3.7/library/telnetlib.html)
+
+### Setup
+- Assumes:
+    - board is a raspberry pi
+    - a pi-hole is running on the board
+    - the referenced e-ink display is hooked up to the board
+- SSH onto board or otherwise access it
+- Pull down this repo
+- Install Python dependencies with `pip3 install -r requirements.txt`
+- Run `python3 pi-display.py`
--- pi-display.py
+++ pi-display.py
@@ -6,12 +6,32 @@ import sys
 import os
 import psutil
 import logging
+import time
 from adafruit_epd.epd import Adafruit_EPD
 from adafruit_epd.ssd1675 import Adafruit_SSD1675
 from telnetlib import Telnet
 
 TELNET_EOM_BYTE_STR = b"---EOM---"
 TELNET_EOM_STR = "---EOM---"
+
+
+"""
+TODO How to get Pi-hole info from both pi-holes? 
+    - Expose telnet api on a lan-visible interface for pi-zero
+        - Would need to forward port 4711 from localhost to something in 192.168.1.x
+        - I think either eth0 or wlan0 would work
+TODO How to get system info from both pi-holes?
+    - Create telnet api and have a python program running on both machines. One asks for perf info
+    via telnet, the other answers
+        - Pi_3b is server, pi zero would be client
+"""
+
+
+def strip_eom(text):
+    if text.endswith(TELNET_EOM_STR):
+        return text[:-len(TELNET_EOM_STR)].strip()
+    else:
+        return text.strip()
 
 
 def setup_logger():
@@ -26,101 +46,105 @@ def setup_logger():
 log = setup_logger()
 
 
-def strip_eom(text):
-    if text.endswith(TELNET_EOM_STR):
-        return text[:-len(TELNET_EOM_STR)].strip()
-    else:
-        return text.strip()
+class PiDisplayController:
 
+    def __init__(self):
+        # create the spi device and pins we will need
+        self.spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        self.ecs = digitalio.DigitalInOut(board.CE0)
+        self.dc = digitalio.DigitalInOut(board.D22)
+        self.rst = digitalio.DigitalInOut(board.D27)
+        self.busy = digitalio.DigitalInOut(board.D17)
+        self.sramcs_pin = None
 
-def get_pihole_version(tn: Telnet):
-    tn.write(b">version")
-    log.info("Getting pihole version...")
-    response = tn.read_until(TELNET_EOM_BYTE_STR).decode('ascii')
-    return strip_eom(response)
+        self.eink = Adafruit_SSD1675(
+            122, 250, self.spi,
+            cs_pin=self.ecs, dc_pin=self.dc, sramcs_pin=self.sramcs_pin,
+            rst_pin=self.rst, busy_pin=self.busy)
+        log.info("Created display")
+        self.eink.rotation = 3
 
+        self.tn = Telnet('127.0.0.1', 4711)
+        log.info("Opened telnet connection")
 
-def get_pihole_stats(tn: Telnet):
-    tn.write(b">stats")
-    log.info("Getting pihole stats...")
-    response = tn.read_until(TELNET_EOM_BYTE_STR).decode('ascii')
-    return strip_eom(response)
+    def get_pihole_version(self, tn: Telnet):
+        tn.write(b">version")
+        log.info("Getting pihole version...")
+        response = tn.read_until(TELNET_EOM_BYTE_STR).decode('ascii')
+        return strip_eom(response)
 
+    def get_pihole_stats(self, tn: Telnet):
+        tn.write(b">stats")
+        log.info("Getting pihole stats...")
+        response = tn.read_until(TELNET_EOM_BYTE_STR).decode('ascii')
+        return strip_eom(response)
 
-"""
-TODO How to get Pi-hole info from both pi-holes? 
-      - Expose telnet api on a lan-visible interface for pi-zero
-          - Would need to forward port 4711 from localhost to something in 192.168.1.x
-          - I think either eth0 or wlan0 would work
-TODO How to get system info from both pi-holes?
-    - Create telnet api and have a python program running on both machines
-        - Pi_3b is server, pi zero would be client
-"""
+    def draw_perf_stats(self):
+        log.info("Clearing display buffer")
+        self.eink.fill(Adafruit_EPD.WHITE)
 
+        version_info = self.get_pihole_version(self.tn)
+        stats = self.get_pihole_stats(self.tn)
+        log.info("Pulled performance data off telnet")
 
-def setup_display():
-    # create the spi device and pins we will need
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    ecs = digitalio.DigitalInOut(board.D12)
-    dc = digitalio.DigitalInOut(board.D11)
+        loadavg = os.getloadavg()
+        log.info("Pulled performance data off os")
 
-    srcs = digitalio.DigitalInOut(board.D10)
-    rst = digitalio.DigitalInOut(board.D9)
-    busy = digitalio.DigitalInOut(board.D5)
+        cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
+        vmem = psutil.virtual_memory()
+        root_disk_usage = psutil.disk_usage('/')
+        temp_info = psutil.sensors_temperatures()
+        boot_time = psutil.boot_time()
+        log.info("Pulled performance data off os")
 
-    log.info("Creating display")
-    display = Adafruit_SSD1675(
-        122, 250, spi,
-        cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs,
-        rst_pin=rst, busy_pin=busy)
+        SEPARATOR = "\n------------------\n"
+        perf_info = (
+            f"{SEPARATOR}"
+            f"pi-hole version info:\n\n{version_info}"
+            f"{SEPARATOR}"
+            f"ad-blocking stats:\n\n{stats}"
+            f"{SEPARATOR}"
+            f"system load avg:{loadavg}"
+            f"{SEPARATOR}"
+            f"cpu usage:{cpu_usage}"
+            f"{SEPARATOR}"
+            f"temp (celsius):{temp_info}"
+            f"{SEPARATOR}"
+            f"memory:{vmem}"
+            f"{SEPARATOR}"
+            f"root disk usage:{root_disk_usage}"
+            f"{SEPARATOR}"
+            f"boot_time:{boot_time}"
+            f"{SEPARATOR}")
+        x = 1
+        y = 1
+        self.eink.text(perf_info, x, y, Adafruit_EPD.BLACK)
+        log.info("Drawing eink buffer")
+        self.eink.display()
 
-    display.rotation = 1
+    def __del__(self):
+        self.tn.close()
+        log.info("Closed telnet connection")
 
-    # clear the buffer
-    log.info("Clear buffer")
-    display.fill(Adafruit_EPD.WHITE)
-    display.pixel(10, 100, Adafruit_EPD.BLACK)
-
-    log.info("Draw text")
-    display.text('hello world', 25, 10, Adafruit_EPD.BLACK)
-    display.display()
+    def wait_for_display_interval(self):
+        DISPLAY_WRITE_INTERVAL_SEC = 60 * 3
+        log.info(
+            f"Waiting for {DISPLAY_WRITE_INTERVAL_SEC} seconds before writing to display")
+        time_waited_sec = 0
+        while time_waited_sec < DISPLAY_WRITE_INTERVAL_SEC:
+            time.sleep(30)
+            time_waited_sec += 30
+            log.info(
+                f"{DISPLAY_WRITE_INTERVAL_SEC - time_waited_sec} seconds left to wait")
 
 
 def main():
     log.info("--------------------------------"
              f"\n{sys.argv[0]}\n"
              "--------------------------------")
-    with Telnet('127.0.0.1', 4711) as tn:
-        version_info = get_pihole_version(tn)
-        stats = get_pihole_stats(tn)
-        cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
-        loadavg = os.getloadavg()
-        vmem = psutil.virtual_memory()
-        root_disk_usage = psutil.disk_usage('/')
-        temp_info = psutil.sensors_temperatures()
-        boot_time = psutil.boot_time()
-        log.info("Closing telnet connection\n")
-
-    SEPARATOR = "\n------------------\n"
-    log.info(
-        f"{SEPARATOR}"
-        f"pi-hole version info:\n\n{version_info}"
-        f"{SEPARATOR}"
-        f"ad-blocking stats:\n\n{stats}"
-        f"{SEPARATOR}"
-        f"system load avg:{loadavg}"
-        f"{SEPARATOR}"
-        f"cpu usage:{cpu_usage}"
-        f"{SEPARATOR}"
-        f"temp (celsius):{temp_info}"
-        f"{SEPARATOR}"
-        f"memory:{vmem}"
-        f"{SEPARATOR}"
-        f"root disk usage:{root_disk_usage}"
-        f"{SEPARATOR}"
-        f"boot_time:{boot_time}"
-        f"{SEPARATOR}"
-    )
+    disp = PiDisplayController()
+    disp.draw_perf_stats()
+    disp.wait_for_display_interval()
 
 
 if __name__ == '__main__':
